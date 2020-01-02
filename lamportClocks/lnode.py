edc355df43275c7ff2a49bb58e91e265f1d95e1d
esther-yyt <yyao63@asu.edu>
2020-01-02 14:14:06 -0700

    comment lnode

--- lamportClocks/lnode.py
+++ lamportClocks/lnode.py
@@ -11,62 +11,78 @@ import lclock
 import eventcounter
 
 class LNode:
-	""" Implementation of a node or process for Lamport Clocks. Uses classes LListener, LClock and EventCounter. """
+	"""
+	Implementation of a node or process for Lamport Clocks.
+
+	"""
 
 	def __init__(self, events, idnum, host, port):
-
+		# init the message size
 		self.msgsize = 1024
-
+		# init the clock
 		self.clock = lclock.LClock()
+		# init the events
 		self.events = events
+		# init the id number
 		self.idnum = idnum
+		# init the host
 		self.host = host
+		# init the port number
 		self.port = port
 
+	# get the id of the node or process
 	def getID(self):
 		return self.idnum
 
+	# get the host of the node or process
 	def getHost(self):
 		return self.host
 
+	# get the port number of the node or process
 	def getPort(self):
 		return self.port
 
 	def listen(self):
+		# init the listener
 		self.listener = llistener.LListener(self.host, self.port, self.events, self.clock)
-		# self.listener.start()
+		# return the listener
 		return self.listener
 
+	# simulate local events
 	def localEvent(self):
-		""" Have a simulated local event, increase Lamport clock by a random amount (1-5). """
+		# increase clock by one
 		self.clock.increment()
+		# increase event counter by one
 		self.events.increment()
-
+		# print clock
 		print("lclock",self.clock.getValue())
 
-
+	# simulate sending message event
 	def send(self, targetid, targethost, targetport):
-		""" Send a message containing sender id and Lamport clock value. """
+		# increase clock by one
 		self.clock.increment()
-
+		# a message containing sender id and Lamport clock value.
 		msg = str(self.idnum) +" "+ str(self.clock.getValue())
-
+		# create the socket
 		self.sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# connect to the target host
 		self.sender.connect((targethost, targetport))
 
 		try:
+			#send message
 			self.sender.sendall(msg)
-			# Printing for sent message.
+			# print the sent message.
 			print("s " + str(targetid) + " " + msg.split()[1])
 
 		except Exception as e:
 			pass
 		finally: 
+			# close the socket
 			self.sender.close()
-
+		#increase the event counter
 		self.events.increment()
 
-
+		# print Lamport clock
 		print("lclock",self.clock.getValue())
 
 
