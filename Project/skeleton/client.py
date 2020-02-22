d73e3810e79acb147bba4b6b80a5b6d18e944c51
maverickjin88 <maverickjin88@gmail.com>
2020-02-22 15:55:19 -0700

    update

--- Project/skeleton/client.py
+++ Project/skeleton/client.py
@@ -1,22 +1,22 @@
 import grpc
-
-# import the generated classes
 import skeleton_pb2
 import skeleton_pb2_grpc
 
 def runEvent(arg):
 
+    # the event needs to be sent to the external process
     if 'dest' in arg:
-        print("sendMSg",arg)
 
+        print('before sending', arg['dest'])
+        # open a gRPC channel
+        channel = grpc.insecure_channel('localhost:5005'+str(arg['dest']))
 
-    # # open a gRPC channel
-    # channel = grpc.insecure_channel('localhost:50051')
-    #
-    # # create a stub (client)
-    # stub = skeleton_pb2_grpc.SkeletonStub(channel)
-    #
-    # # create a valid request message
-    # event = skeleton_pb2.Events(id=1,name="test",dependency="test2",localclock=1)
-    # # make the call
-    # response = stub.SendMsg(event)
+        # create a stub (client)
+        stub = skeleton_pb2_grpc.SkeletonStub(channel)
+
+        # create a valid request message
+        event = skeleton_pb2.Events(id=1,name="test",dependency="test2",localclock=1)
+        # make the call
+        response = stub.SendMsg(event)
+        print('msgSent', response)
+
