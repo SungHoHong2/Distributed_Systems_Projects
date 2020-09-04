e2904d9a7de9b569f438943f218711d7899e929e
maverickjin88 <maverickjin88@gmail.com>
2020-09-03 22:48:06 -0700

    update

--- Project/skeleton/TestApplication.py
+++ Project/skeleton/TestApplication.py
@@ -10,15 +10,23 @@ import sys
 
 # dictionary for dependencies
 dpdict = {
-    'query_receive' : 'query_request',
-    'query_return':'query_reply',
-    'deposit_receive': 'deposit_request',
-    'deposit_return': 'deposit_reply',
-    'withdraw_receive': 'withdraw_request',
-    'withdraw_return': 'withdraw_reply',
+    'query_receive'     :   'query_request',
+    'query_execute'     :   'query_receive',
+    'query_reply'       :   'query_execute',
+    'query_return'      :   'query_reply',
+
+    'deposit_receive'   :   'deposit_request',
+    'deposit_execute'   :   'deposit_receive',
+    'deposit_reply'     :   'deposit_execute',
+    'deposit_return'    :   'deposit_reply',
+
+    'withdraw_receive'  :   'withdraw_request',
+    'withdraw_execute'  :   'withdraw_receive',
+    'withdraw_reply'    :   'withdraw_execute',
+    'withdraw_return'   :   'withdraw_reply',
 }
-# each process keeps a dependency link
-recvMsg = list()
+# each process keeps a private message buffers
+sendMsg, execMsg, recvMsg = list(), list(), list()
 
 # server
 class ExampleServicer(example_pb2_grpc.ExampleServicer):
@@ -36,10 +44,25 @@ class ExampleServicer(example_pb2_grpc.ExampleServicer):
         # return the fake object
         return response
 
-def worker(obj,readyQueue):
+def transaction(balance, event):
+    if event['name'] == 'query_execute' and balance >= event['money']:
+        return event['money']
+    elif event['name'] == 'deposit_execute':
+        return balance + event['money']
+    elif event['name'] == 'withdraw_execute' and balance >= event['money']:
+        return balance - event['money']
+    else:
+        print('wrong result detected!', event, balance)
+        return -1
 
-    # each process runs the server as a thread
+def worker(obj,readyQueue):
+    # get the id of the process
     id = obj['name'][1:]
+    # get the type of the process
+    type = obj['type']
+    # create a balance for bank processes
+    if type == 'bank':
+        balance = obj['balance']
 
     # create a gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -58,38 +81,69 @@ def worker(obj,readyQueue):
     # generate the events
     for i in range(0, len(obj['events'])):
         # print(obj['name'], obj['events'][i])
-        arg = obj['events'][i]
-        # print(id, arg)
+        event = obj['events'][i]
+        # print(id, event)
         # the event is sending the message to external
-        if 'dest' in arg:
+        if 'dest' in event:
             # get the destination
-            channel = grpc.insecure_channel('localhost:5005'+str(arg['dest']))
+            channel = grpc.insecure_channel('localhost:5005'+str(event['dest']))
             # create a stub (client)
             stub = example_pb2_grpc.ExampleStub(channel)
-            # craft a message
-            event = example_pb2.Event(id=arg['id'],name=arg['name'],money=0)
-            # send and receive a message
-            stub.MsgDelivery(event)
 
-        if 'recv' in arg:
+            if event['name'] in dpdict:
+               depFound = False
+               while not(depFound):
+                    time.sleep(1)
+                    for i in range(len(sendMsg)):
+                        if sendMsg[i]['name'] == dpdict[event['name']]:
+                            msg = sendMsg.pop(i)
+                            depFound = True
+                            event['money'] = msg['money']
+                            break
+            print('FRISK',id, event)
+            # craft a message
+            msg = example_pb2.Event(id=event['id'],name=event['name'],money= event['money'])
+            # send and receive a message
+            stub.MsgDelivery(msg)
+
+        if 'recv' in event:
             # wait for the reply function
-            # print(id, arg['name'], 'waiting for ...', dpdict[arg['name']])
+            # print(id, event, 'waiting for ...', dpdict[event['name']])
             depFound = False
             while not(depFound):
                 time.sleep(1)
                 for i in range(len(recvMsg)):
-                    if recvMsg[i]['name'] == dpdict[arg['name']]:
-                        # FIXME: Execute the operation
-                        print(id,'execute operation', recvMsg[i]['name'])
+                    if recvMsg[i]['name'] == dpdict[event['name']]:
+                        msg = recvMsg.pop(i)
+                        depFound = True
+                        event['money'] = msg['money']
+                        print('FRISK',id, event)
+                        execMsg.append(event)
+                        break
 
-                        recvMsg.pop(i)
+        if 'exec' in event:
+            # print(id, event, 'waiting for ...', dpdict[event['name']])
+            depFound = False
+            while not(depFound):
+                time.sleep(1)
+                for i in range(len(execMsg)):
+                    if execMsg[i]['name'] == dpdict[event['name']]:
+                        msg = execMsg.pop(i)
+                        event['money'] = msg['money']
+                        print('FRISK',id, event)
+                        balance = transaction(balance, event)
+                        sendMsg.append(event)
                         depFound = True
                         break
+            # get the money from the file
 
     # set the status to finish
     readyQueue[int(id)-1] = 2
     print(id,"Function END!")
     waitWorker(2, readyQueue)
+    if type == 'bank':
+        print(id, 'balance', balance)
+
 
 def waitWorker(type, queue):
     allSet = True
